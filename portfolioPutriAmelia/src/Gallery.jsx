900e8f37160c01403eb60a3d8a73ff496ad55ed8
JOSELUIS <josevegas.marquez@gmail.com>
2025-12-09 08:38:56 -0500

    implementando Gallery

---
+++ portfolioPutriAmelia/src/Gallery.jsx
@@ -0,0 +1,12 @@
+import { photoSessions } from '/data/photos';
+
+function Gallery() {
+    return (
+        <div className="gallery-grid">
+            {photoSessions.map(photo => (
+                <div key={photo.id} className="gallery-item">
+                    <img src={photo.src} alt={photo.title} />
+                    {/* Opcional: Mostrar título o tags al hacer hover */}
+                </div>
+            )
+}
---
+++ portfolioPutriAmelia/src/data/photos.js
@@ -0,0 +1,5 @@
+export const photoSessions = [
+    { id: 1, title: 'Urban Editorial', src: 'https://pixabay.com/get/gd32a5518119ce1a943053c726ffb00de55780983068c0ca4c8d52550dbce5d197adf1f77e770c2659acb1700213e4cc4ffb1e452551d5a23ebc2c91b1e1ad807_1920.jpg', tags: ['fashion', 'urban'] },
+    { id: 2, title: 'Haute Couture', src: 'https://pixabay.com/get/g0885b1cf57fa743a16bd5a959cfe3f81fdc5120f6f18f8e3c18fdfda4f2e3f8f11aa6415df210ec896a163bbd909f71d0c540642108d973661fd2fc4266d06d1_1920.jpg', tags: ['couture'] },
+    // ... más sesiones
+];
